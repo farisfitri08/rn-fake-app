f328f924c4a644500afa3bebca0e0df4e8887f17
farisfitri08 <farisfitri08@gmail.com>
2022-09-22 04:04:16 +0800

    testing

--- pages/screen/HomePage.js
+++ pages/screen/HomePage.js
@@ -72,7 +72,7 @@ function HomePage({ navigation }) {
             color: "white",
           }}
         >
-          START NOW
+          START NOW PL
         </Text>
       </TouchableHighlight>
       <TouchableHighlight
